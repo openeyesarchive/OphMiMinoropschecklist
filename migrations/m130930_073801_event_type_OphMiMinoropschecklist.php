72565cde4d3bf848e74c7f3d9c18b41aed71f5f6
Mark <m@rkw.me>
2013-09-30 08:40:53 +0100

    initial commit

---
+++ migrations/m130930_073801_event_type_OphMiMinoropschecklist.php
@@ -0,0 +1,78 @@
+<?php 
+class m130930_073801_event_type_OphMiMinoropschecklist extends CDbMigration
+{
+	public function up()
+	{
+		// --- EVENT TYPE ENTRIES ---
+
+		// create an event_type entry for this event type name if one doesn't already exist
+		if (!$this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name'=>'OphMiMinoropschecklist'))->queryRow()) {
+			$group = $this->dbConnection->createCommand()->select('id')->from('event_group')->where('name=:name',array(':name'=>'Miscellaneous'))->queryRow();
+			$this->insert('event_type', array('class_name' => 'OphMiMinoropschecklist', 'name' => 'Minor ops checklist','event_group_id' => $group['id']));
+		}
+		// select the event_type id for this event type name
+		$event_type = $this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name'=>'OphMiMinoropschecklist'))->queryRow();
+
+		// --- ELEMENT TYPE ENTRIES ---
+
+		// create an element_type entry for this element type name if one doesn't already exist
+		if (!$this->dbConnection->createCommand()->select('id')->from('element_type')->where('name=:name and event_type_id=:eventTypeId', array(':name'=>'Time out',':eventTypeId'=>$event_type['id']))->queryRow()) {
+			$this->insert('element_type', array('name' => 'Time out','class_name' => 'Element_OphMiMinoropschecklist_TimeOut', 'event_type_id' => $event_type['id'], 'display_order' => 1));
+		}
+		// select the element_type_id for this element type name
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:eventTypeId and name=:name', array(':eventTypeId'=>$event_type['id'],':name'=>'Time out'))->queryRow();
+
+
+
+		// create the table for this element type: et_modulename_elementtypename
+		$this->createTable('et_ophmiminoropschecklist_timeout', array(
+				'id' => 'int(10) unsigned NOT NULL AUTO_INCREMENT',
+				'event_id' => 'int(10) unsigned NOT NULL',
+				'patient_name_checked' => 'tinyint(1) unsigned NOT NULL', // Patient name checked
+				'procedure_planned' => 'tinyint(1) unsigned NOT NULL', // Procedure planned
+				'site_marked_x' => 'tinyint(1) unsigned NOT NULL', // Site marked x
+				'last_modified_user_id' => 'int(10) unsigned NOT NULL DEFAULT 1',
+				'last_modified_date' => 'datetime NOT NULL DEFAULT \'1901-01-01 00:00:00\'',
+				'created_user_id' => 'int(10) unsigned NOT NULL DEFAULT 1',
+				'created_date' => 'datetime NOT NULL DEFAULT \'1901-01-01 00:00:00\'',
+				'PRIMARY KEY (`id`)',
+				'KEY `et_ophmiminoropschecklist_timeout_lmui_fk` (`last_modified_user_id`)',
+				'KEY `et_ophmiminoropschecklist_timeout_cui_fk` (`created_user_id`)',
+				'KEY `et_ophmiminoropschecklist_timeout_ev_fk` (`event_id`)',
+				'CONSTRAINT `et_ophmiminoropschecklist_timeout_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)',
+				'CONSTRAINT `et_ophmiminoropschecklist_timeout_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
+				'CONSTRAINT `et_ophmiminoropschecklist_timeout_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)',
+			), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin');
+
+	}
+
+	public function down()
+	{
+		// --- drop any element related tables ---
+		// --- drop element tables ---
+		$this->dropTable('et_ophmiminoropschecklist_timeout');
+
+
+
+
+		// --- delete event entries ---
+		$event_type = $this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name'=>'OphMiMinoropschecklist'))->queryRow();
+
+		foreach ($this->dbConnection->createCommand()->select('id')->from('event')->where('event_type_id=:event_type_id', array(':event_type_id'=>$event_type['id']))->queryAll() as $row) {
+			$this->delete('audit', 'event_id='.$row['id']);
+			$this->delete('event', 'id='.$row['id']);
+		}
+
+		// --- delete entries from element_type ---
+		$this->delete('element_type', 'event_type_id='.$event_type['id']);
+
+		// --- delete entries from event_type ---
+		$this->delete('event_type', 'id='.$event_type['id']);
+
+		// echo "m000000_000001_event_type_OphMiMinoropschecklist does not support migration down.\n";
+		// return false;
+		echo "If you are removing this module you may also need to remove references to it in your configuration files\n";
+		return true;
+	}
+}
+
