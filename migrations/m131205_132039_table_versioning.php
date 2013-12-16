aabd0a8961f1e324599258f872c7b5d4da201b4c
Mark <m@rkw.me>
2013-12-16 12:17:23 +0000

    [OE-3856] migration consolidation

--- migrations/m131205_132039_table_versioning.php
+++ migrations/m131205_132039_table_versioning.php
@@ -6,22 +6,22 @@ class m131205_132039_table_versioning extends CDbMigration
 	{
 		$this->execute("
 CREATE TABLE `et_ophmiminoropschecklist_timeout_version` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `event_id` int(10) unsigned NOT NULL,
-  `patient_name_checked` tinyint(1) unsigned NOT NULL,
-  `procedure_planned` tinyint(1) unsigned NOT NULL,
-  `site_marked_x` tinyint(1) unsigned NOT NULL,
-  `last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  `created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
-  `created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
-  PRIMARY KEY (`id`),
-  KEY `acv_et_ophmiminoropschecklist_timeout_lmui_fk` (`last_modified_user_id`),
-  KEY `acv_et_ophmiminoropschecklist_timeout_cui_fk` (`created_user_id`),
-  KEY `acv_et_ophmiminoropschecklist_timeout_ev_fk` (`event_id`),
-  CONSTRAINT `acv_et_ophmiminoropschecklist_timeout_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophmiminoropschecklist_timeout_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `acv_et_ophmiminoropschecklist_timeout_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`event_id` int(10) unsigned NOT NULL,
+	`patient_name_checked` tinyint(1) unsigned NOT NULL,
+	`procedure_planned` tinyint(1) unsigned NOT NULL,
+	`site_marked_x` tinyint(1) unsigned NOT NULL,
+	`last_modified_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`last_modified_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	`created_user_id` int(10) unsigned NOT NULL DEFAULT '1',
+	`created_date` datetime NOT NULL DEFAULT '1901-01-01 00:00:00',
+	PRIMARY KEY (`id`),
+	KEY `acv_et_ophmiminoropschecklist_timeout_lmui_fk` (`last_modified_user_id`),
+	KEY `acv_et_ophmiminoropschecklist_timeout_cui_fk` (`created_user_id`),
+	KEY `acv_et_ophmiminoropschecklist_timeout_ev_fk` (`event_id`),
+	CONSTRAINT `acv_et_ophmiminoropschecklist_timeout_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophmiminoropschecklist_timeout_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `acv_et_ophmiminoropschecklist_timeout_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 		");
 
@@ -36,10 +36,16 @@ CREATE TABLE `et_ophmiminoropschecklist_timeout_version` (
 		$this->addColumn('et_ophmiminoropschecklist_timeout_version','version_id','int(10) unsigned NOT NULL');
 		$this->addPrimaryKey('version_id','et_ophmiminoropschecklist_timeout_version','version_id');
 		$this->alterColumn('et_ophmiminoropschecklist_timeout_version','version_id','int(10) unsigned NOT NULL AUTO_INCREMENT');
+
+		$this->addColumn('et_ophmiminoropschecklist_timeout','deleted','tinyint(1) unsigned not null');
+		$this->addColumn('et_ophmiminoropschecklist_timeout_version','deleted','tinyint(1) unsigned not null');
 	}
 
 	public function down()
 	{
+		$this->dropColumn('et_ophmiminoropschecklist_timeout','deleted');
+		$this->dropColumn('et_ophmiminoropschecklist_timeout_version','deleted');
+
 		$this->dropTable('et_ophmiminoropschecklist_timeout_version');
 	}
 }
