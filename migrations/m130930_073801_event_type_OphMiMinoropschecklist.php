905d6dc06f08d79a0e4f017d7cffe0b77cde2d93
Mark <m@rkw.me>
2014-01-15 08:03:00 +0000

    [OE-3914] fixed collation

--- migrations/m130930_073801_event_type_OphMiMinoropschecklist.php
+++ migrations/m130930_073801_event_type_OphMiMinoropschecklist.php
@@ -42,7 +42,7 @@ class m130930_073801_event_type_OphMiMinoropschecklist extends CDbMigration
 				'CONSTRAINT `et_ophmiminoropschecklist_timeout_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)',
 				'CONSTRAINT `et_ophmiminoropschecklist_timeout_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
 				'CONSTRAINT `et_ophmiminoropschecklist_timeout_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)',
-			), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin');
+			), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci');
 
 	}
 
